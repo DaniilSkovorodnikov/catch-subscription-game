a391b828c8eb361e2cf97c0ffe2cb06ac3ea2d47
dskovorodnikov <dskovorodnikov@naumen.ru>
2025-12-18 11:03:05 +0500

    ++

--- src/FinishMenu.jsx
+++ src/FinishMenu.jsx
@@ -2,7 +2,7 @@ export default function FinishMenu() {
   return (
     <div className='menu-container'>
       <h1>Игра закончена!</h1>
-      <p>Чтобы получить, сообщи мне в тг код: SOSOK</p>
+      <p>Чтобы получить приз, сообщи мне в тг код: SOSOK</p>
       <p>Чтобы начать игру заново, обнови страницу!</p>
     </div>
   )
